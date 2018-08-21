9b83a7efc011a84a8da408b49a6a71041f601cc3
kraxarn <emil202821@hotmail.com>
2018-08-21 17:32:00 +0200

    Fix instant freeze

--- VirtualGuzla/main.cpp
+++ VirtualGuzla/main.cpp
@@ -3,17 +3,15 @@
 int main(int argc, char** argv)
 {
 	// Load engine
-	cout << "Loading engine..." << endl;
-	Engine(false);
+	Engine(true);
 
 	// Load window
-	cout << "Creating window..." << endl;
 	auto window = Window(Window::windowed, Vector2<int>(1280, 720), true);
 
 	// Main loop
-	cout << "Entering main loop..." << endl;
 	while (true)
 	{
-		
+		window.Clear();
+		window.Flip();
 	}
 }
